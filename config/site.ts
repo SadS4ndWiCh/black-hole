e22b3a11075b34459db498658694edc46c166551
sads4ndwich <caiocamargo007@gmail.com>
2023-05-28 15:58:21 -0300

    feat(site): change site configs

--- config/site.ts
+++ config/site.ts
@@ -1,18 +1,10 @@
 export type SiteConfig = typeof siteConfig
 
 export const siteConfig = {
-  name: "Next.js",
-  description:
-    "Beautifully designed components built with Radix UI and Tailwind CSS.",
-  mainNav: [
-    {
-      title: "Home",
-      href: "/",
-    },
-  ],
+  name: "Black Hole",
+  description: "A simple personal password manager",
   links: {
-    twitter: "https://twitter.com/shadcn",
-    github: "https://github.com/shadcn/ui",
-    docs: "https://ui.shadcn.com",
+    twitter: "https://twitter.com/SadSAndWiCh_",
+    github: "https://github.com/SadS4ndWiCh/black-hole",
   },
 }
